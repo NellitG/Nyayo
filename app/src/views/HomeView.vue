5e57e44f5bb9d00f58bd3814e130584dfcbd45dc
NellitG <kamaunelson157@gmail.com>
2024-06-28 18:52:44 +0300

    message

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -10,7 +10,7 @@
 import Hero from "@/components/Hero.vue";
 import Clients from "@/components/Clients.vue";
 // import icons from "@/components/icons.vue";
-// import Faq from "@/components/Faq.vue";
+import Faq from "@/components/Faq.vue";
 import Cards from "@/components/Cards.vue";
 // import Card2 from "@/components/Card2.vue";
 </script>
