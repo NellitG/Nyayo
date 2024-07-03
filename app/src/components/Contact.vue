efbc0fb26c3ecd081d0a3ce790e3bf9423174965
NellitG <kamaunelson157@gmail.com>
2024-07-03 21:51:43 +0300

    message

--- app/src/components/Contact.vue
+++ app/src/components/Contact.vue
@@ -1,58 +1,60 @@
 <template>
-    <div class="bg-white">
-    <div class="flex justify-center h-screen">
-        <div class="hidden bg-cover lg:block lg:w-2/3" style="background-image: url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)">
-            <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
-                <div>
-                    <h2 class="text-2xl font-bold text-white sm:text-3xl">Meraki UI</h2>
-
-                    <p class="max-w-xl mt-3 text-gray-300">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. In
-                        autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam temporibus
-                        molestiae
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
-            <div class="flex-1">
-                <div class="text-center">
-                    <div class="flex justify-center mx-auto">
-                        <img class="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt="">
-                    </div>
-
-                    <p class="mt-3 text-gray-500">Sign in to access your account</p>
-                </div>
-
-                <div class="mt-8">
-                    <form>
-                        <div>
-                            <label for="email" class="block mb-2 text-sm text-gray-600">Email Address</label>
-                            <input type="email" name="email" id="email" placeholder="example@example.com" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                        </div>
-
-                        <div class="mt-6">
-                            <div class="flex justify-between mb-2">
-                                <label for="password" class="text-sm text-gray-600">Password</label>
-                                <a href="#" class="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline">Forgot password?</a>
-                            </div>
-
-                            <input type="password" name="password" id="password" placeholder="Your Password" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
-                        </div>
-
-                        <div class="mt-6">
-                            <button class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
-                                Sign in
-                            </button>
-                        </div>
-
-                    </form>
-
-                    <p class="mt-6 text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="#" class="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</a>.</p>
-                </div>
-            </div>
-        </div>
+    <div class="flex min-h-screen md:w-screen w-full items-center justify-center text-gray-600">
+    <div class="relative">
+      
+  <div class="hidden sm:block h-56 w-56 text-amber-300 absolute a-z-10 -left-20 -top-20">
+     <svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='40' height='40' patternTransform='scale(0.6) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='none'/><path d='M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5'  stroke-width='1' stroke='none' fill='currentColor'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(#a)'/></svg>
     </div>
-</div>
+  <div class="hidden sm:block h-28 w-28 text-amber-300 absolute a-z-10 -right-20 -bottom-20">
+     <svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='b' patternUnits='userSpaceOnUse' width='40' height='40' patternTransform='scale(0.5) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='none'/><path d='M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5'  stroke-width='1' stroke='none' fill='currentColor'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(#b)'/></svg>
+    </div>
+      <!-- Register -->
+      <div class="relative flex flex-col sm:w-[30rem] rounded-lg border-gray-400 bg-white shadow-lg px-4">
+        <div class="flex-auto p-6">
+          <!-- Logo -->
+          <div class="mb-10 flex flex-shrink-0 flex-grow-0 items-center justify-center overflow-hidden">
+            <a href="#" class="flex items-center gap-2 text-amber-500 hover:text-amber-500">
+              <span class="flex-shrink-0 text-3xl font-semibold tracking-tight opacity-100">Call Us Today</span>
+            </a>
+          </div>
+          <!-- /Logo -->
+          <h4 class="mb-2 font-medium text-gray-950 xl:text-xl">Welcome to NellitG!</h4>
+          <p class="mb-6 text-gray-500">Please sign-in to access your account</p>
+  
+          <form id="" class="mb-4" action="#" method="POST">
+            <div class="mb-4">
+              <label for="email" class="mb-2 inline-block text-xs font-medium uppercase text-gray-700">Email or Username</label>
+              <input type="text" class="block w-full cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-amber-500 focus:bg-white focus:text-gray-950 focus:shadow" id="email" name="email-username" placeholder="Enter your email or username" />
+            </div>
+            <div class="mb-4">
+              <div class="flex justify-between">
+                <label class="mb-2 inline-block text-xs font-medium  text-gray-950" for="password">Password</label>
+                <a href="auth-forgot-password-basic.html" class="cursor-pointer text-amber-500 no-underline hover:text-amber-500">
+                  <small class=" ">Forgot Password?</small>
+                </a>
+              </div>
+              <div class="relative flex w-full flex-wrap items-stretch">
+                <input type="password" id="password" class="relative block flex-auto cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-amber-500 focus:bg-white focus:text-gray-600 focus:shadow" name="password" placeholder="············" />
+              </div>
+            </div>
+            <div class="mb-4">
+              <div class="block">
+              
+                <label class="inline-block" for="remember-me"> Remember Me </label>
+              </div>
+            </div>
+            <div class="mb-4">
+              <button class="grid w-full cursor-pointer select-none rounded-md border border-amber-500 bg-amber-500 py-2 px-5 text-center align-middle text-sm text-white shadow hover:border-yellow-600 hover:bg-yellow-500 hover:text-white focus:text-white focus:shadow-none" type="submit">Sign in</button>
+            </div>
+          </form>
+  
+          <p class="mb-4 text-center">
+            New on NellitG?
+            <a href="#" class="cursor-pointer text-amber-500 no-underline hover:text-yellow-500"> Create an account </a>
+          </p>
+        </div>
+      </div>
+      <!-- /Register -->
+    </div>
+  </div>
 </template>
