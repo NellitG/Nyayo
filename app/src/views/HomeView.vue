cef44f421b1f54c9e5c968d1490806e0776de4fc
NellitG <kamaunelson157@gmail.com>
2024-07-03 21:50:31 +0300

    message

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -4,13 +4,13 @@
   <!-- <Clients/> -->
   <!-- <icons/> -->
   <!-- <Card2/> -->
-  <Faq />
+  <!-- <Faq /> -->
 </template>
 <script setup>
 import Hero from "@/components/Hero.vue";
-// import Clients from "@/components/Clients.vue";
+import Clients from "@/components/Clients.vue";
 // import icons from "@/components/icons.vue";
-import Faq from "@/components/Faq.vue";
+// import Faq from "@/components/Faq.vue";
 import Cards from "@/components/Cards.vue";
 import Card2 from "@/components/Card2.vue";
 </script>
