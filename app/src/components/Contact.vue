db2e07e2c8b389b74a04eee4c14907d93633887c
NellitG <kamaunelson157@gmail.com>
2024-10-16 20:02:39 +0300

    message

--- app/src/components/Contact.vue
+++ app/src/components/Contact.vue
@@ -1,61 +1,134 @@
 <template>
-    <div class=" flex flex-shrink-0 flex-grow-0 items-center justify-center overflow-hidden">
-            <a href="#" class="flex items-center gap-2 text-amber-500 hover:text-amber-500">
-              <span class="flex-shrink-0 text-5xl font-semibold tracking-tight opacity-100">Contact Us</span>
-            </a>
+   <!-- ====== Contact Section Start -->
+<!-- Contact -->
+<div class="max-w-7xl px-4  lg:px-8 py-12 lg:py-24 mx-auto">
+    <div class="mb-6 sm:mb-10 max-w-2xl text-center mx-auto">
+      <h2 class="font-medium text-black text-2xl sm:text-4xl">Talk to Us</h2>
+    </div>
+  
+    <div
+      class="grid grid-cols-1 lg:grid-cols-2 lg:items-center gap-6 md:gap-8 lg:gap-12"
+    >
+      <div
+        class="aspect-w-16 aspect-h-6 lg:aspect-h-14 overflow-hidden bg-gray-100 rounded-2xl"
+      >
+        <img
+          class="group-hover:scale-105 transition-transform duration-500 ease-in-out object-cover rounded-2xl"
+          src="https://images.unsplash.com/photo-1572021335469-31706a17aaef?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Image Description"
+        />
+      </div>
+      <!-- End Col -->
+  
+      <div class="space-y-8 lg:space-y-16">
+        <div>
+          <h3 class="mb-5 font-semibold text-black">Our address</h3>
+  
+          <!-- Grid -->
+          <div class="grid sm:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-12">
+            <div class="flex gap-4">
+              <svg
+                class="flex-shrink-0 size-5 text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"></path>
+                <circle cx="12" cy="10" r="3"></circle>
+              </svg>
+  
+              <div class="grow">
+                <p class="text-sm text-gray-600">United Kingdom</p>
+                <address class="mt-1 text-black not-italic">
+                  300 Bath Street, Tay House<br />
+                  Glasgow G2 4JR
+                </address>
+              </div>
+            </div>
           </div>
-    <div class="flex min-h-screen md:w-screen w-full items-center justify-center text-gray-600">
-    <div class="relative">
-      
-  <div class="hidden sm:block h-56 w-56 text-amber-300 absolute a-z-10 -left-20 -top-20">
-     <svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='40' height='40' patternTransform='scale(0.6) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='none'/><path d='M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5'  stroke-width='1' stroke='none' fill='currentColor'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(#a)'/></svg>
-    </div>
-  <div class="hidden sm:block h-28 w-28 text-amber-300 absolute a-z-10 -right-20 -bottom-20">
-     <svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='b' patternUnits='userSpaceOnUse' width='40' height='40' patternTransform='scale(0.5) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='none'/><path d='M11 6a5 5 0 01-5 5 5 5 0 01-5-5 5 5 0 015-5 5 5 0 015 5'  stroke-width='1' stroke='none' fill='currentColor'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(#b)'/></svg>
-    </div>
-      <!-- Register -->
-      <div class="relative flex flex-col sm:w-[30rem] rounded-lg border-gray-400 bg-white shadow-lg px-4">
-        <div class="flex-auto p-6">
-          <!-- Logo -->
-          
-          <!-- /Logo -->
-          <h4 class="mb-2 font-medium text-gray-950 xl:text-xl">Welcome to NellitG!</h4>
-          <p class="mb-6 text-gray-500">Please sign-in to access your account</p>
+          <!-- End Grid -->
+        </div>
   
-          <form id="" class="mb-4" action="#" method="POST">
-            <div class="mb-4">
-              <label for="email" class="mb-2 inline-block text-xs font-medium uppercase text-gray-700">Email or Username</label>
-              <input type="text" class="block w-full cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-amber-500 focus:bg-white focus:text-gray-950 focus:shadow" id="email" name="email-username" placeholder="Enter your email or username" />
-            </div>
-            <div class="mb-4">
-              <div class="flex justify-between">
-                <label class="mb-2 inline-block text-xs font-medium  text-gray-950" for="password">Password</label>
-                <a href="auth-forgot-password-basic.html" class="cursor-pointer text-amber-500 no-underline hover:text-amber-500">
-                  <small class=" ">Forgot Password?</small>
-                </a>
-              </div>
-              <div class="relative flex w-full flex-wrap items-stretch">
-                <input type="password" id="password" class="relative block flex-auto cursor-text appearance-none rounded-md border border-gray-400 bg--100 py-2 px-3 text-sm outline-none focus:border-amber-500 focus:bg-white focus:text-gray-600 focus:shadow" name="password" placeholder="············" />
-              </div>
-            </div>
-            <div class="mb-4">
-              <div class="block">
-              
-                <label class="inline-block" for="remember-me"> Remember Me </label>
-              </div>
-            </div>
-            <div class="mb-4">
-              <button class="grid w-full cursor-pointer select-none rounded-md border border-amber-500 bg-amber-500 py-2 px-5 text-center align-middle text-sm text-white shadow hover:border-yellow-600 hover:bg-yellow-500 hover:text-white focus:text-white focus:shadow-none" type="submit">Sign in</button>
-            </div>
-          </form>
+        <div>
+          <h3 class="mb-5 font-semibold text-black">Our contacts</h3>
   
-          <p class="mb-4 text-center">
-            New on NellitG?
-            <a href="#" class="cursor-pointer text-amber-500 no-underline hover:text-yellow-500"> Create an account </a>
-          </p>
+          <!-- Grid -->
+          <div class="grid sm:grid-cols-2 gap-4 sm:gap-6 md:gap-8 lg:gap-12">
+            <div class="flex gap-4">
+              <svg
+                class="flex-shrink-0 size-5 text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path
+                  d="M21.2 8.4c.5.38.8.97.8 1.6v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V10a2 2 0 0 1 .8-1.6l8-6a2 2 0 0 1 2.4 0l8 6Z"
+                ></path>
+                <path d="m22 10-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 10"></path>
+              </svg>
+  
+              <div class="grow">
+                <p class="text-sm text-gray-600">Email us</p>
+                <p>
+                  <a
+                    class="relative inline-block font-medium text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 hover:before:bg-black focus:outline-none focus:before:bg-black"
+                    href="mailto:example@site.so"
+                  >
+                    hello@example.so
+                  </a>
+                </p>
+              </div>
+            </div>
+  
+            <div class="flex gap-4">
+              <svg
+                class="flex-shrink-0 size-5 text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path
+                  d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"
+                />
+              </svg>
+  
+              <div class="grow">
+                <p class="text-sm text-gray-600">Call us</p>
+                <p>
+                  <a
+                    class="relative inline-block font-medium text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:w-full before:h-1 before:bg-lime-400 hover:before:bg-black focus:outline-none focus:before:bg-black"
+                    href="mailto:example@site.so"
+                  >
+                    +44 222 777-000
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <!-- End Grid -->
         </div>
       </div>
-      <!-- /Register -->
+      <!-- End Col -->
     </div>
   </div>
+  <!-- End Contact -->
+
 </template>
