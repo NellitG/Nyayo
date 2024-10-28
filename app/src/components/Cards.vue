253b55f81651e2a35e6489d31c4e43e7d66a6163
NellitG <kamaunelson157@gmail.com>
2024-10-28 20:28:57 +0300

    message

--- app/src/components/Cards.vue
+++ app/src/components/Cards.vue
@@ -18,10 +18,6 @@
                         </div>
                         <div class="py-2 px-9">
 
-                          <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 group-hover:fill-white">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                            </svg> -->
-
                             <h3 class="mt-8 text-3xl font-semibold text-black group-hover:text-white">Laravel
                             </h3>
                             <p class="mt-4 text-base text-black group-hover:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil, quos, illo voluptates qui dolore, rerum laboriosam esse maiores soluta nam corrupti veritatis id earum. Explicabo quaerat facere velit iure amet.
@@ -35,9 +31,7 @@
                             class=" absolute bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-black group-hover:w-1/2  ">
                         </div>
                         <div class="py-2 px-9 relative  ">
-                        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 group-hover:fill-white">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-                        </svg> -->
+                     
 
 
                             <h3 class="mt-8 text-3xl font-semibold text-black group-hover:text-white">React
