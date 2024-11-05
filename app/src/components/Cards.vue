331151891d0e496438240ddcbb4bc4ea7ff9c762
NellitG <kamaunelson157@gmail.com>
2024-11-05 04:46:43 +0300

    message

--- app/src/components/Cards.vue
+++ app/src/components/Cards.vue
@@ -1,117 +1,35 @@
 <template>
-  <section class="bg-white">
-    <div class="py-10 sm:py-16 block lg:py-20 relative bg-opacity-50">
-        <div
-            class="relative mx-auto h-full px-4 pb-20 md:pb-0 sm:max-w-xl md:max-w-full md:px-24 lg:max-w-screen-xl lg:px-8">
+    <div class="max-w-4xl mx-auto space-y-24 py-16">
+      <div class="mt-10 relative bg-cover bg-center rounded-lg shadow-lg overflow-hidden" 
+           style="background-image: url('https://img.freepik.com/free-photo/medium-shot-smiley-kid-sitting-outdoors_23-2148860402.jpg?ga=GA1.1.487577296.1727511220&semt=ais_hybrid'); min-height: 300px;">
+        
+        <!-- Overlay -->
+        <div class="bg-black bg-opacity-40 h-full w-full absolute inset-0"></div>
+        
+        <!-- Content on top of the background image and overlay -->
+        <div class="relative z-10 p-8 text-white flex items-center justify-center flex-col text-center">
+          <h2 class="text-5xl font-bold mb-4">Join Our Action!</h2>
+          <h2 class="text-5xl font-semibold mb-6">Everyone Can Help.</h2>
+          
+          <!-- Buttons -->
+          <div class="flex space-x-4">
+            <router-link to="/getinvolved">
+              <button class="bg-yellow-400 text-gray-950 font-semibold px-6 py-3 transition duration-300">
+                Donate
+              </button>
+            </router-link>
+            <router-link to="/getinvolved">
+              <button class="bg-indigo-950 text-white font-semibold px-6 py-3 transition duration-300">
+                Become a Volunteer
+              </button>
+            </router-link>
+          </div>
         </div>
-            <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 relative">
-                <div class="max-w-xl mx-auto text-center">
-                    <div class="inline-flex px-4 py-1.5 mx-auto rounded-full">
-                    </div>
-                    <p class="mt-4 text-3xl leading-relaxed text-black group-hover:text-black">Our Best Pracices Focuses On</p>
-                </div>
-                <div class="grid grid-cols-1 gap-2 mt-12 sm:grid-cols-3 lg:mt-20">
-                    <a href="#"
-                        class="transition-all duration-1000 bg-white hover:bg-amber-600 border hover:shadow-xl m-2 p-4 relative group">
-                        <div
-                            class="absolute bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-black group-hover:w-1/2">
-                        </div>
-                        <div class="py-2 px-9">
-
-                            <h3 class="mt-8 text-3xl font-semibold text-black group-hover:text-white">Laravel
-                            </h3>
-                            <p class="mt-4 text-base text-black group-hover:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil, quos, illo voluptates qui dolore, rerum laboriosam esse maiores soluta nam corrupti veritatis id earum. Explicabo quaerat facere velit iure amet.
-                            </p>
-                        </div>
-                    </a>
-
-                    <a href="#"
-                        class="transition-all duration-1000 bg-white hover:bg-yellow-600 border hover:shadow-xl m-2 p-4 relative group  ">
-                        <div
-                            class=" absolute bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-black group-hover:w-1/2  ">
-                        </div>
-                        <div class="py-2 px-9 relative  ">
-                     
-
-
-                            <h3 class="mt-8 text-3xl font-semibold text-black group-hover:text-white">React
-                            </h3>
-                            <p class="mt-4 text-base text-black group-hover:text-white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque, cupiditate perspiciatis vel explicabo repellendus deserunt adipisci reiciendis voluptatum et distinctio asperiores voluptas dicta ullam quas aliquam ad blanditiis ducimus accusantium.</p>
-                        </div>
-                    </a>
-
-                     <a href="#"
-                        class="transition-all duration-1000 bg-white hover:bg-yellow-600 border hover:shadow-xl m-2 p-4 relative group  ">
-                        <div
-                            class=" absolute  bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-black group-hover:w-1/2  ">
-                        </div>
-                        <div class="py-2 px-9 relative  ">
-                        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 group-hover:fill-white">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-                        </svg> -->
-
-
-                            <h3 class="mt-8 text-3xl font-semibold text-black group-hover:text-white">Angular
-                            </h3>
-                            <p class="mt-4 text-base text-black group-hover:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aliquam odio doloremque saepe reprehenderit totam alias eum, vel pariatur debitis, veritatis quidem dicta minus tenetur harum corrupti, laudantium ut et.</p>
-                        </div>
-                    </a>
-
-                     <a href="#"
-                        class="transition-all duration-1000 bg-white hover:bg-yellow-600 border hover:shadow-xl m-2 p-4 relative group  ">
-                        <div
-                            class=" absolute  bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-black group-hover:w-1/2  ">
-                        </div>
-                        <div class="py-2 px-9 relative  ">
-                        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 group-hover:fill-white">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-                        </svg> -->
-
-
-                            <h3 class="mt-8 text-3xl font-semibold text-black group-hover:text-white">Vue
-                            </h3>
-                            <p class="mt-4 text-base text-black group-hover:text-white">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio exercitationem quibusdam vero autem doloremque voluptatum pariatur qui eius a nam explicabo accusantium reiciendis corporis libero neque quis incidunt, beatae esse?</p>
-                        </div>
-                    </a>
-
-                     <a href="#"
-                        class="transition-all duration-1000 bg-white hover:bg-yellow-600 border hover:shadow-xl m-2 p-4 relative group  ">
-                        <div
-                            class=" absolute  bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-black group-hover:w-1/2  ">
-                        </div>
-                        <div class="py-2 px-9 relative  ">
-                        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 group-hover:fill-white">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
-                        </svg> -->
-
-
-                            <h3 class="mt-8 text-3xl font-semibold text-black group-hover:text-white">Tailwind
-                            </h3>
-                            <p class="mt-4 text-base text-black group-hover:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ipsa reprehenderit sed reiciendis temporibus facilis aliquid iste a iure? Dignissimos ipsam quibusdam reprehenderit nesciunt iste sapiente unde alias distinctio modi?</p>
-                        </div>
-                    </a>
-
-                    <a href="#"
-                        class="transition-all duration-1000 bg-white hover:bg-yellow-600 border hover:shadow-xl m-2 p-4 relative group  ">
-                        <div
-                            class=" absolute  bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-black group-hover:w-1/2  ">
-                        </div>
-                        <div class="py-2 px-9 relative">
-                        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
-                        </svg> -->
-
-                            <h3 class="mt-8 text-3xl font-semibold text-black group-hover:text-white">Java
-                        </h3>
-                            <p class="mt-4 text-base text-black group-hover:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, numquam placeat autem ipsa ducimus ratione minus et sed eum veniam, eius deleniti dolores hic saepe voluptas minima quibusdam necessitatibus animi.</p>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    
-</section>
-</template>
-<script></script>
-<style scoped></style>
+      </div>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped></style>
+  
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -3,11 +3,14 @@
   <Mission />
   <calltoaction />
   <Hero />
+  <Cards />
 </template>
 <script setup>
 import Hero from "@/components/Hero.vue";
 import Mission from "@/components/Mission.vue";
 import Button from "@/components/Button.vue";
 import calltoaction from "@/components/calltoaction.vue";
+import Cards from "@/components/Cards.vue";
+
 </script>
 <style lang="scss" scoped></style>
