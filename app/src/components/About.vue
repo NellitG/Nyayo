41973b210f2684065529584efad75dbc40f89db0
NellitG <kamaunelson157@gmail.com>
2024-11-05 05:00:19 +0300

    message

--- app/src/components/About.vue
+++ app/src/components/About.vue
@@ -1,27 +1,68 @@
 <template >
-  <section class="mt-32">
-    <div class="max-w-7xl mx-auto px-2">
-      <div class="max-w-full ">
+  <div class="max-w-full mt-24">
         <div class="max-w-3xl mx-auto py-30 space-y-5">
         <h1 class="text-3xl md:text-5xl text-primary-700 font-bold text-center text-indigo-950">
           Who Are We
         </h1>
-        <p class="text-lg text-gray-900">
-          Nyayo Inc was established in 2024 in Nairobi, Kenya, with a passionate commitment to empowering differently-abled children through access to digital education.
-          The NGO was founded to address the educational and social disparities faced by children with disabilities in underserved communities.
-          Recognizing that these children often lack the resources and support to thrive in conventional educational settings, Nyayo Inc aims to bridge this gap by providing inclusive, accessible, and technology-driven learning opportunities.
-          Since its inception, the organization has grown rapidly, partnering with schools and communities to foster an environment of inclusivity.
-        </p>
       </div>
     </div>
-      
-       <div class="container px-6 py-10 mx-auto">
+ <div class="m-8 p-5">
+  <ol class="border-l border-neutral-300 dark:border-neutral-500 md:flex md:justify-center md:gap-6 md:border-l-0 md:border-t">
+  
+    <!--First item-->
+    <li >
+      <div class="flex-start flex items-center pt-2 md:block md:pt-0">
+        <div
+          class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500 md:-mt-[5px] md:ml-0 md:mr-0"></div>
+       
+      </div>
+      <div class="ml-4 mt-2 pb-5 md:ml-0">
+        <h4 class="mb-1.5 text-xl font-semibold">Title of section 1</h4>
+        <p class="mb-3 text-neutral-500 dark:text-neutral-300">
+          Nyayo Inc was established in 2024 in Nairobi, Kenya, with a passionate commitment to empowering differently-abled children through access to digital education.
+        </p>
+      </div>
+    </li>
+  
+    <!--Second item-->
+    <li>
+      <div class="flex-start flex items-center pt-2 md:block md:pt-0">
+        <div
+          class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500 md:-mt-[5px] md:ml-0 md:mr-0"></div>
+      </div>
+      <div class="ml-4 mt-2 pb-5 md:ml-0">
+        <h4 class="mb-1.5 text-xl font-semibold">Title of section 2</h4>
+        <p class="mb-3 text-neutral-500 dark:text-neutral-300">
+          The NGO was founded to address the educational and social disparities faced by children with disabilities in underserved communities.<br>
+          
+        </p>
+      </div>
+    </li>
+  
+    <!--Third item-->
+    <li>
+      <div class="flex-start flex items-center pt-2 md:block md:pt-0">
+        <div
+          class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500 md:-mt-[5px] md:ml-0 md:mr-0"></div>
+       
+      </div>
+      <div class="ml-4 mt-2 pb-5 md:ml-0">
+        <h4 class="mb-1.5 text-xl font-semibold">Title of section 3</h4>
+        <p class="mb-3 text-neutral-500 dark:text-neutral-300">
+          Recognizing that these children often lack the resources and support to thrive in conventional educational settings, Nyayo Inc aims to bridge this gap by providing inclusive, accessible, and technology-driven learning opportunities.
+        </p>
+      </div>
+    </li>
+  
+</ol>
+</div>
+<div class="container px-6 py-10 mx-auto">
         <div class="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-2">
             <div>
                 <img class="relative z-10 object-cover w-full rounded-md h-96" src="https://img.freepik.com/free-photo/close-up-african-kids-reading-together_23-2148860425.jpg?ga=GA1.1.487577296.1727511220&semt=ais_hybrid" alt="">
                   <div class="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow">
                     <a href="#" class="font-semibold text-gray-950 hover:underline text-3xl">
-                      Learning
+                      Commitment
                     </a>
 
                     <p class="mt-3 text-sm text-gray-500 md:text-sm">
@@ -35,7 +76,7 @@
 
                 <div class="relative z-20 max-w-lg p-6 mx-auto -mt-20 bg-white rounded-md shadow">
                     <a href="#" class="font-semibold text-gray-950 text-4xl hover:underline">
-                      Commitment
+                      Learning
                     </a>
 
                     <p class="mt-3 text-sm text-gray-500 md:text-sm">
@@ -45,6 +86,4 @@
             </div>
         </div>
     </div>
-    </div>
-  </section>
 </template>
