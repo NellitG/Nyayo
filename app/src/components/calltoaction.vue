9d83ececc938bb39af98b08dffa9f8cf69c5a085
NellitG <kamaunelson157@gmail.com>
2024-11-05 03:16:57 +0300

    message

--- app/src/components/calltoaction.vue
+++ app/src/components/calltoaction.vue
@@ -1,3 +1,63 @@
 <template>
-    
-</template>
+    <div class="bg-white py-10 ">
+  
+      <div class=" m-4 text-center p-2 py-10  max-w-7xl mx-auto  ">
+        <div  class=" space-y-7 py-5">
+          <!-- <h3 class="font-semibold text-6xl">Features of FINANCIAL</h3> -->
+          <!-- <p class="px-10">We take pride in being a trusted partner for all of your financial needs.</p> -->
+        </div>
+        <div
+          class="flex flex-col gap-5 pt-4 md:flex-row justify-center items-center m-2 p-2">
+          <div
+            class="flex flex-col items-center bg-white rounded-lg border-2 border-black-400 hover:shadow-xl space-y-4 md:max-w-auto p-4">
+            <div class="shadow-lg hover:shadow-xl p-3 ">
+              <img
+                class="shrink-0 h-12 w-12"
+                src="../assets/sacco9.svg"
+                alt="sacco1 image"
+              />
+            </div>
+            <h4 class="font-semibold">Becoming a Volunteer</h4>
+            <p>
+              Thought the lorem ipsum was suppossed to be an opium related task
+              to get high on life.
+            </p>
+          </div>
+          <div
+            class="flex flex-col items-center bg-white rounded-lg border-2 border-black-400 hover:shadow-xl space-y-4 md:max-w-auto p-4">
+          <div class="shadow-lg hover:shadow-xl p-3">
+              <img
+                class="shrink-0 h-12 w-12 "
+                src="../assets/sacco8.svg"
+                alt="sacco2 image"
+              />
+            </div>
+            <h4 class="font-semibold">Quick Fundraise</h4>
+            <p>
+              Thought the lorem ipsum was suppossed to to be am opium related task
+              to get high on life
+            </p>
+            
+          </div>
+          <div
+            class="flex flex-col items-center bg-white rounded-lg border-2 border-black-400 hover:shadow-xl space-y-4 md:max-w-auto p-4"
+          >
+            <div class="shadow-lg hover:shadow-xl p-3">
+              <img
+                class="shrink-0 h-12 w-12"
+                src="../assets/sacco7.svg"
+                alt="sacco3 image"
+              />
+            </div>
+            <h4 class="font-semibold">Start Donating</h4>
+            <p>
+              Thought the lorem ipsum was suppossed to to be am opium related task
+              to get high on life
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  <script></script>
+  <style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,19 +1,13 @@
 <template>
- <Button />
+  <Button />
   <Mission />
-   <Hero />
-   <calltoaction />
+  <calltoaction />
+  <Hero />
 </template>
 <script setup>
 import Hero from "@/components/Hero.vue";
 import Mission from "@/components/Mission.vue";
 import Button from "@/components/Button.vue";
 import calltoaction from "@/components/calltoaction.vue";
-
-// import Clients from "@/components/Clients.vue";
-// import icons from "@/components/icons.vue";
-// import Faq from "@/components/Faq.vue";
-// import Cards from "@/components/Cards.vue";
-// import Card2 from "@/components/Card2.vue";
 </script>
 <style lang="scss" scoped></style>
