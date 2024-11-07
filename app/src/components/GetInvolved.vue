6d9b811db18770d1fb879aa94121f79316acfc17
NellitG <kamaunelson157@gmail.com>
2024-11-07 15:27:11 +0300

    message

--- app/src/components/GetInvolved.vue
+++ app/src/components/GetInvolved.vue
@@ -70,7 +70,7 @@
                 <button
                   class="w-full px-4 py-2 mt-6 tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:bg-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-80"
                 >
-                  Start Now
+                  Donate
                 </button>
               </div>
               <div class="text-center mt-8">
