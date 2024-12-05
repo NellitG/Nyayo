d7c7e2d0612bc1406eb224f2caa1477f042dc832
NellitG <kamaunelson157@gmail.com>
2024-12-05 23:03:33 +0300

    message

--- app/src/main.js
+++ app/src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import router from "./router";
 import App from "./App.vue";
 
+
 /**styling */
 // import "./style.css";
 import "./assets/tailwind.css"; //tailwind
