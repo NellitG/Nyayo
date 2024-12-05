5cf88d139b9c84880ab43262e1bda26639229741
NellitG <kamaunelson157@gmail.com>
2024-12-05 15:08:15 +0300

    message

--- app/src/components/Events.vue
+++ app/src/components/Events.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="bg-white p-6 rounded-lg shadow-md">
             <img
-              src="../assets/Nyayo's Foundation Poster.png"
+              src="../assets/events.png"
               alt="Happy Customers"
               class="w-full rounded-md mb-6"
             />
