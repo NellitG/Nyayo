027a3f234f1ced14cf1535c73e78f69621f81eb0
NellitG <kamaunelson157@gmail.com>
2024-12-31 01:08:36 +0300

    message

--- app/src/components/Contact.vue
+++ app/src/components/Contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-0 mt-40">
+  <div class="py-0 mt-32">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="text-center">
         <h1 class="mt-2 text-3xl font-semibold text-blue-900 text-gray-950">
