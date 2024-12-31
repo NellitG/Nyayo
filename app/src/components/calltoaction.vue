d045bab36ef542af83f67ce65b0e882c1f1f68ec
NellitG <kamaunelson157@gmail.com>
2024-12-31 12:18:37 +0300

    message

--- app/src/components/calltoaction.vue
+++ app/src/components/calltoaction.vue
@@ -1,55 +1,60 @@
 <template>
-    <div class="bg-white py-10 mt-0 lg:mt-10 mr-4">
-      <div class="mb-0 text-center p-2 py-10 max-w-7xl mx-auto">
+  <div class="bg-white py-10 mt-0 lg:mt-10 mr-4">
+    <div class="mb-0 text-center p-2 py-10 max-w-7xl mx-auto">
+      <div
+        class="flex flex-col gap-5 pt-4 md:flex-row justify-center items-center m-2 p-2"
+      >
         <div
-          class="flex flex-col gap-5 pt-4 md:flex-row justify-center items-center m-2 p-2">
-          <div
-            class="flex flex-col items-center bg-white rounded-lg hover:border-yellow-400 border-2 border-black-400 hover:shadow-xl space-y-4 md:max-w-auto p-4">
-            <div class="shadow-lg hover:shadow-xl p-3 ">
-              <img
-                class="shrink-0 h-12 w-12"
-                src="../assets/sacco9.svg"
-                alt="sacco1 image"
-              />
-            </div>
-            <h4 class="font-semibold">Becoming a Volunteer</h4>
-            <p>
-              Become a part of the Volunteer team that takes the initiative to help children achieve their dreams.
-            </p>
-          </div>
-          <div
-            class="flex flex-col items-center bg-white rounded-lg hover:border-yellow-400 border-2 border-black-400 hover:shadow-xl space-y-4 md:max-w-auto p-4">
+          class="flex flex-col items-center bg-white rounded-lg hover:border-yellow-400 border-2 border-black-400 hover:shadow-xl space-y-4 md:max-w-auto p-4"
+        >
           <div class="shadow-lg hover:shadow-xl p-3">
-              <img
-                class="shrink-0 h-12 w-12 "
-                src="../assets/sacco8.svg"
-                alt="sacco2 image"
-              />
-            </div>
-            <h4 class="font-semibold">Quick Fundraise</h4>
-            <p>
-              Help support our mission of providing digital education tools to differently-abled children.
-            </p>
-            
+            <img
+              class="shrink-0 h-12 w-12"
+              src="../assets/sacco9.svg"
+              alt="sacco1 image"
+            />
           </div>
-          <div
-            class="flex flex-col items-center bg-white rounded-lg hover:border-yellow-400 border-2 border-black-400 hover:shadow-xl space-y-4 md:max-w-auto p-4"
-          >
-            <div class="shadow-lg hover:shadow-xl p-3">
-              <img
-                class="shrink-0 h-12 w-12"
-                src="../assets/sacco7.svg"
-                alt="sacco3 image"
-              />
-            </div>
-            <h4 class="font-semibold">Start Donating</h4>
-            <p>
-              Collaborate to foster inclusivity and advocate for policy changes that benefit differently-abled individuals.
-            </p>
+          <h4 class="font-semibold">Becoming a Volunteer</h4>
+          <p class="text-center">
+            Every action matters. Become part of a movement that uplifts lives,
+            VOLUNTEER with us. Together, we can make inclusion a global standard
+            and transform futures, one child at a time.
+          </p>
+        </div>
+        <div
+          class="flex flex-col items-center bg-white rounded-lg hover:border-yellow-400 border-2 border-black-400 hover:shadow-xl space-y-4 md:max-w-auto p-4"
+        >
+          <div class="shadow-lg hover:shadow-xl p-3">
+            <img
+              class="shrink-0 h-12 w-12"
+              src="../assets/sacco8.svg"
+              alt="sacco2 image"
+            />
           </div>
+          <h4 class="font-semibold">Quick Fundraise</h4>
+          <p>
+            Your involvement fuels our mission to uplift and empower children with special needs. Together, we achieve the extraordinary. Every penny counts in achieving the future we envision.
+          </p>
+        </div>
+        <div
+          class="flex flex-col items-center bg-white rounded-lg hover:border-yellow-400 border-2 border-black-400 hover:shadow-xl space-y-4 md:max-w-auto p-4"
+        >
+          <div class="shadow-lg hover:shadow-xl p-3">
+            <img
+              class="shrink-0 h-12 w-12"
+              src="../assets/sacco7.svg"
+              alt="sacco3 image"
+            />
+          </div>
+          <h4 class="font-semibold">Start Donating</h4>
+          <p>
+            Collaborate to foster inclusivity and advocate for policy changes
+            that benefit differently-abled individuals.
+          </p>
         </div>
       </div>
     </div>
-  </template>
-  <script></script>
-  <style scoped></style>
+  </div>
+</template>
+<script></script>
+<style scoped></style>
